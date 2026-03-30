7a8b4073ccc0737bf566b099a37b0b36e0679b30
hubdemarcel <marcel.julien@sdintelligence.info>
2026-03-30 06:37:13 -0600

    fix: add serverExternalPackages for Prisma in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: require("path").join(__dirname),
+  serverExternalPackages: ["@prisma/client", "prisma"],
 };
 module.exports = nextConfig;
